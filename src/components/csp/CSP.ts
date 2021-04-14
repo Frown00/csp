df6c57d3f8e8e806da8934c8614cd79852ad59a1
Fabian Kuriata <f.kuriata@t-bull.com>
2021-04-14 15:49:33 +0200

    save

--- src/components/csp/CSP.ts
+++ src/components/csp/CSP.ts
@@ -1,14 +1,14 @@
 import _ from "lodash";
 import { IVariable } from "./types";
 
-type State<V, D> = { variable: IVariable<V,D>, domain: D[] }[];
+type State<V> = { variable: IVariable<V>, domain: any[][] }[];
 
-export class CSP<V, D> {
-  private variables: IVariable<V, D>[]
+export class CSP<V> {
+  private variables: IVariable<V>[]
   private constraints: (() => boolean)[]
-  private domains: D[][]
-  private solutions:  IVariable<V, D>[][]
-  private state: State<V, D>
+  private domains: any[][]
+  private solutions:  IVariable<V>[][]
+  private state: State<V>
 
   constructor() {
     this.variables = [];
@@ -24,32 +24,39 @@ export class CSP<V, D> {
     return null;
   }
 
+  getRandomSolution() {
+    if(this.solutions.length > 0) {
+      const random = Math.floor(Math.random() * this.solutions.length);
+      return this.solutions[random];
+    }
+    return null;
+  }
+
   getSolutionsCount() {
     return this.solutions.length;
   }
 
-  addVariable(variable: { id: V, value: D }, domain: D[]) {
+  addVariable(variable: { id: V, value: any[] }) {
     this.variables.push(variable);
-    this.domains.push(domain);
-    this.state.push({ 
-      variable: _.cloneDeep(variable), 
-      domain: _.cloneDeep(domain) 
-    });
   }
 
-  addConstraint(constraint: () => boolean) {
-    this.constraints.push(constraint);
+  addDomain<D>(domain: D[]) {
+    this.domains.push(domain);
+  }
+
+  addConstraints(...constraints: (() => boolean)[]) {
+    this.constraints.push(...constraints);
   }
 
   backtracing() {
-    this.backtrace(0);
+    this.backtrace(0, 0);
     if(this.solutions.length <= 0) {
       return false;
     }
     return true;
   }
 
-  private backtrace(i: number) {
+  private backtrace(i: number, d: number) {
     if(i === this.variables.length) {
       if(this.isSafe()) {
         // solution
@@ -58,13 +65,24 @@ export class CSP<V, D> {
       }
       return false;
     }
-    for(let c = 0; c < this.domains[i].length; c++) {
-      this.variables[i].value = this.domains[i][c];
+    const variable = this.variables[i];
+    const domain = this.domains[d];
+    for(let v = 0; v < domain.length; v++) {
+      variable.value[d] = domain[v];
       if(this.isSafe()) {
-        if(this.backtrace(i + 1))
-          return true;
-      } else {
-        this.variables[i].value = null;
+        if(d < this.domains.length - 1) {
+          if(this.backtrace(i, d + 1)) {
+            return true;
+          } else {
+            variable.value[d] = null;
+          }
+        } else {
+          if(this.backtrace(i + 1, 0)) {
+            return true;
+          } else {
+            variable.value[d] = null;
+          }
+        }
       }
     }
     return false;
--- src/components/csp/types.d.ts
+++ src/components/csp/types.d.ts
@@ -1,4 +1,4 @@
-export interface IVariable<T, V> {
+export interface IVariable<T> {
   id: T,
-  value: V
+  value: any[]
 }
